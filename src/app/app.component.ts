028eaaf8409c805bd54be60cfbb5c69a40094ea9
Pedro Amaral <amaralph1995@gmail.com>
2023-04-19 17:31:41 -0300

    create initializer and basic properties of AppComponentClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'reactive-forms-project';
+export class AppComponent implements OnInit {
+  statusOptions = ['stable', 'critical', 'finished']
+  projectData: FormGroup;
+
+  ngOnInit() {
+    this.projectData = new FormGroup({
+      'projectName': new FormControl(null),
+      'mail': new FormControl(null),
+      'status': new FormControl('stable')
+    })
+  }
 }

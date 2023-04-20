a641594c882492051a1ea3c537e1b7738a66f05c
Pedro Amaral <amaralph1995@gmail.com>
2023-04-20 19:03:19 -0300

    add error messages to project name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,18 +7,26 @@ import {FormControl, FormGroup} from "@angular/forms";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  statusOptions = ['stable', 'critical', 'finished']
+  statusOptions = ['stable', 'critical', 'finished'];
+  invalidProjectNames = ['Test'];
   projectForm!: FormGroup;
 
   ngOnInit() {
     this.projectForm = new FormGroup({
-      'projectName': new FormControl(null),
-      'mail': new FormControl(null),
+      'projectName': new FormControl(null, [Validators.required, this.invalidNames.bind(this)]),
+      'mail': new FormControl(null, [Validators.required, Validators.email]),
       'status': new FormControl('stable')
     })
   }
 
   onSubmit() {
-    console.log(this.projectForm)
+    console.log(this.projectForm.value);
+  }
+
+  invalidNames(control: FormControl): { [p: string]: boolean } | null {
+    if (this.invalidProjectNames.indexOf(control.value) !== -1) {
+      return {'invalidProjectName': true};
+    }
+    return null;
   }
 }

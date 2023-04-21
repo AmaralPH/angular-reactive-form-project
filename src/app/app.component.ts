a184f964eaff3a9b29caa711834cf9ee1c3cc24a
Pedro Amaral <amaralph1995@gmail.com>
2023-04-20 21:18:44 -0300

    create an async version of validation function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.projectForm = new FormGroup({
-      'projectName': new FormControl(null, [Validators.required, this.invalidNames.bind(this)]),
+      'projectName': new FormControl(null, [Validators.required], this.invalidNamesAsync),
       'mail': new FormControl(null, [Validators.required, Validators.email]),
       'status': new FormControl('stable')
     })
@@ -28,5 +29,18 @@ export class AppComponent implements OnInit {
       return {'invalidProjectName': true};
     }
     return null;
+  }
+
+  invalidNamesAsync(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'Test') {
+          resolve({'invalidProjectName': true})
+        } else {
+          resolve(null)
+        }
+      }, 1500)
+    })
+    return promise;
   }
 }
